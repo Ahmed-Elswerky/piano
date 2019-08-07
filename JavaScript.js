a1887ff1076fb75cb6ada0b0bb8e47b47d4d4e34
Ahmed Elswerkey <khawater11212@gmail.com>
2019-08-07 13:31:58 +0200

    interface enhanced

--- JavaScript.js
+++ JavaScript.js
@@ -1,22 +1,22 @@
 
 //typing style
 var space_name = document.getElementById("piano_p_pre"), piano_p = document.getElementById("p"), str = "This is a Beta version of Audioid's Piano.";
-document.onload = setTimeout(function () {
-    var iii = 0;
-    piano_p.innerHTML = str[iii];
-    string_out(piano_p, iii);
-}, 3000);
+// document.onload = setTimeout(function () {
+//     var iii = 0;
+//     piano_p.innerHTML = str[iii];
+//     string_out(piano_p, iii);
+// }, 3000);
 
-function string_out(place, iii) {
-    iii++;
-    setTimeout(function () {
-        place.innerHTML += str[iii];
-    }, 300);
+// function string_out(place, iii) {
+//     iii++;
+//     setTimeout(function () {
+//         place.innerHTML += str[iii];
+//     }, 300);
 
-    if (str[iii + 1] != undefined)
-        setTimeout(function () { string_out(place, iii); }, 100);
-    else setTimeout(function () { space_name.style.display= "none"; }, 3000);
-}
+//     if (str[iii + 1] != undefined)
+//         setTimeout(function () { string_out(place, iii); }, 100);
+//     else setTimeout(function () { space_name.style.display= "none"; }, 3000);
+// }
 // document.getElementById("explain_i").onmouseover = function(){ex_mouse('over');};
 // document.getElementById("explain_d").onmouseover = function(){ex_mouse('over');};
 // document.getElementById("explain_d").onmouseleave = function(){ex_mouse('leave');};
